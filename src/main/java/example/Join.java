ff7e30af69550e4271c74ced3f59b4642328658c
Rajendra Kadam <Rajendra Kadam>
2019-05-16 10:33:54 +0530

    My First Neo4j UDF

--- src/main/java/example/Join.java
+++ src/main/java/example/Join.java
@@ -2,6 +2,11 @@ package example;
 
 import java.util.List;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.logging.Log;
+import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
@@ -9,8 +14,14 @@ import org.neo4j.procedure.UserFunction;
 /**
  * This is an example how you can create a simple user-defined function for Neo4j.
  */
-public class Join
-{
+public class Join {
+
+    @Context
+    public GraphDatabaseService db;
+
+    @Context
+    public Log log;
+
     @UserFunction
     @Description("example.join(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
     public String join(
@@ -20,5 +31,23 @@ public class Join
             return null;
         }
         return String.join(delimiter, strings);
+    }
+
+    @UserFunction
+    @Description("example.join(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
+    public Long sumNums(@Name("numbers") List<Long> numbers) {
+        if (numbers == null) {
+            return null;
+        }
+        return numbers.stream().mapToLong(Long::longValue).sum();
+    }
+
+    @UserFunction
+    @Description("example.join(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
+    public Node getCompanyByProperty(@Name("key") String key, @Name("value") String value) {
+        if (key == null || value == null) {
+            return null;
+        }
+        return db.findNode(Label.label("Company"), key, value);
     }
 }
